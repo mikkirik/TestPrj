fb7bc592564a7051125069873d27528028270d59
Kirill Mikheev <65455894+mikkirik@users.noreply.github.com>
2023-10-05 22:41:04 +0300

    Authorization

--- xxx_o.py
+++ xxx_o.py
@@ -21,6 +21,42 @@ coordinates = {
     1: (2, 0), 2: (2, 1), 3: (2, 2)}
 
 #победные комбинации
+wins = [[[1, 1, 1],
+         [0, 0, 0],
+         [0, 0, 0]],
+        [[0, 0, 0],
+         [1, 1, 1],
+         [0, 0, 0]],
+        [[0, 0, 0],
+         [0, 0, 0],
+         [1, 1, 1]],
+        [[1, 0, 0],
+         [1, 0, 0],
+         [1, 0, 0]],
+        [[0, 1, 0],
+         [0, 1, 0],
+         [0, 1, 0]],
+        [[0, 0, 1],
+         [0, 0, 1],
+         [0, 0, 1]],
+        [[1, 0, 0],
+         [0, 1, 0],
+         [0, 0, 1]],
+        [[0, 0, 1],
+         [0, 1, 0],
+         [1, 0, 0]]]
+
+
+def win_check(check_field):
+    #функция проверки на выигрышную комбинацию
+    Res = [[], [], []]
+    for i in range(len(wins)):
+        for j in range(len(check_field)):
+            Res[j] = list(map(lambda x,y: x*y, check_field[j], wins[i][j]))
+        if Res == wins[i]:
+            return True
+    return False
+
 
 #Напечатаем пустое поле
 for row in field:
@@ -30,34 +66,44 @@ for row in field:
 print('Давай сыграем в игру!\nИспользуй для выбора квадрата цифровую клавиатуру')
 
 gamer = 'X' #первый всегда ходит Х
+count = 0
 
-while True:
+while count < 9:
     #выбор квадратика - ввод с клавиатуры
-    square = coordinates[int(input(f'Сделай свой ход, мистер {gamer}: '))]
+    num = int(input(f'Сделай свой ход, мистер {gamer}: '))
 
     #если квадрат пустой - заполняем его, иначе передаём ход, нефиг ошибаться
-    if field[square[0]][square[1]] == '-':
-        field[square[0]][square[1]] = gamer
-        if gamer == 'X':
-            x_field[square[0]][square[1]] = 1
+    if 1 <= num <= 9:
+        square = coordinates[num]
+        if (field[square[0]][square[1]] == '-'):
+            field[square[0]][square[1]] = gamer
+            if gamer == 'X':
+                x_field[square[0]][square[1]] = 1
+            else:
+                o_field[square[0]][square[1]] = 1
+            count += 1
         else:
-            o_field[square[0]][square[1]] = 1
+            print('Плохой выбор( Переход хода')
     else:
-        print('Плохой выбор( Переход хода')
+        print('Очень плохой выбор')
+        if exit_function():
+            print('Ну и иди!')
+            break
+        else:
+            print('Отлично, продолжаем!')
 
     #печать поля
     for row in field:
         print(' '.join(row))
-    for row in x_field:
-        print(' '.join(map(str,row)))
-    for row in o_field:
-        print(' '.join(map(str,row)))
+
+    if win_check(x_field) or win_check(o_field):
+        print(f'Игрок {gamer} победил! Поздравляю!')
+        break
 
     #смена игрока
     gamer = 'O' if gamer == 'X' else 'X'
 
-    if exit_function():
-        break
+
 
 
 
